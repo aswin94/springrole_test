24bae3453d24a19575a3594df323b9fa9d0a293e
Aswin Varghese <ashwin.vargheese@gmail.com>
2019-05-21 17:30:28 +0530

    Updates to App included Routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// import Home from './Home'
+import userCard from './userCard'
+import Main from './Main'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="home">
+                    <Switch>
+                        <Route exact path = '/' component = {Main} />
+                        <Route exact path = '/user/:id' component = {userCard} />
+                    </Switch>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withRouter(App);
